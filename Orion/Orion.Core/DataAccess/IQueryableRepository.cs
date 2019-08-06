104374bc468f411ff8993572d70a37282aae7bf6
Goker <gokerakce@outlook.com>
2019-08-06 17:34:37 +0100

    Added repository and entity interfaces

---
+++ Orion/Orion.Core/DataAccess/IQueryableRepository.cs
@@ -0,0 +1,14 @@
+﻿using Orion.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orion.Core.DataAccess
+{
+    public interface IQueryableRepository<T> where T: class, IEntity, new()
+    {
+        IQueryable Table { get; }
+    }
+}
