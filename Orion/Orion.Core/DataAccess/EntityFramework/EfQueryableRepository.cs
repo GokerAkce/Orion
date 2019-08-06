092f11ae24ee1e148007a75a72804266d91127a0
Goker <gokerakce@outlook.com>
2019-08-06 17:47:37 +0100

    Added queryable repository for EF

---
+++ Orion/Orion.Core/DataAccess/EntityFramework/EfQueryableRepository.cs
@@ -0,0 +1,37 @@
+﻿using Orion.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orion.Core.DataAccess.EntityFramework
+{
+    class EfQueryableRepository<T> : IQueryableRepository<T> where T : class, IEntity, new()
+    {
+        private DbContext _context;
+        IDbSet<T> _entities;
+
+        public EfQueryableRepository(DbContext context)
+        {
+            _context = context;
+        }
+
+        public IQueryable Table => this._entities;
+
+        protected virtual IDbSet<T> Entities
+        {
+            get
+            {
+                if(_entities == null)
+                {
+                    _entities = _context.Set<T>();
+                }
+
+                return _entities;
+            }
+        }
+        
+    }
+}
