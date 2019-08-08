8526a5cb1439c528e4ca9267a407572fa15e05bf
Goker <gokerakce@outlook.com>
2019-08-08 16:58:06 +0100

    Added product validator class sample

---
+++ Orion/Orion.Northwind.Business/ValidationRules/FluentValidation/ProductValidator.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+using Orion.Northwind.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orion.Northwind.Business.ValidationRules.FluentValidation
+{
+    class ProductValidator : AbstractValidator<Product>
+    {
+        public ProductValidator()
+        {
+            RuleFor(p => p.ProductName).NotEmpty();
+            RuleFor(p => p.CategoryId).NotEmpty();
+            RuleFor(p => p.UnitPrice).GreaterThan(0);
+            RuleFor(p => p.QuantityPerUnit).NotEmpty();
+            RuleFor(p => p.ProductName).Length(2, 25);
+            //RuleFor(p => p.ProductName).Must(StartWithSmt);
+        }
+
+        private bool StartWithSmt(string arg)
+        {
+            return arg.StartsWith("SOMETHING");
+        }
+    }
+}
