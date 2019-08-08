0e1081fe1c878a2ca0af4cd8244148c73db9a469
Goker <gokerakce@outlook.com>
2019-08-08 23:01:54 +0100

    Added fluent validation aspect tests

---
+++ Orion/Orion.Northwind.Business.Tests/ProductManagerTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Orion.Northwind.DataAccess.Abstract;
+using Orion.Northwind.Business.Concrete.Manager;
+using Orion.Northwind.Entities.Concrete;
+using FluentValidation;
+
+namespace Orion.Northwind.Business.Tests
+{
+    /// <summary>
+    /// Summary description for ProductManagerTests
+    /// </summary>
+    [TestClass]
+    public class ProductManagerTests
+    {
+        [ExpectedException(typeof(ValidationException))]
+        [TestMethod]
+        public void Product_validation_check()
+        {
+            Mock<IProductDal> mock = new Mock<IProductDal>();
+            ProductManager productManager = new ProductManager(mock.Object);
+
+            productManager.Add(new Product());
+        }
+      
+    }
+}
