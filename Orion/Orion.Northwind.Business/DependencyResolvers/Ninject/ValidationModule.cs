d472acd9df6b5b4204d2d3c6e70ecb4d126cf99c
Goker <gokerakce@outlook.com>
2019-08-15 23:07:19 +0100

    Added validation module for dependency injection

---
+++ Orion/Orion.Northwind.Business/DependencyResolvers/Ninject/ValidationModule.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using Ninject.Modules;
+using Orion.Northwind.Business.ValidationRules.FluentValidation;
+using Orion.Northwind.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orion.Northwind.Business.DependencyResolvers.Ninject
+{
+    public class ValidationModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IValidator<Product>>().To<ProductValidator>().InSingletonScope();
+        }
+    }
+}
